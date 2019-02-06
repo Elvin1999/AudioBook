5265948cdf38da01feb0080ba5b92a395818466c
camalzade_elvin <camalzade_elvin@outlook.com>
2019-02-06 21:33:15 +0400

    Done

--- AudioBook1/Form1.Designer.cs
+++ AudioBook1/Form1.Designer.cs
@@ -58,7 +58,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(669, 450);
+            this.ClientSize = new System.Drawing.Size(695, 490);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.listViewPdfs);
             this.Name = "Form1";
--- AudioBook1/Form1.cs
+++ AudioBook1/Form1.cs
@@ -12,8 +12,9 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using iTextSharp.text.pdf.parser;
 using System.Speech;
+using System.Drawing.Imaging;
+using Bytescout.PDFExtractor;
 using System.Speech.Synthesis;
-
 namespace AudioBook1
 {
     public partial class Form1 : Form
@@ -30,6 +31,7 @@ namespace AudioBook1
             var Info = new FileInfo(mydata[0]);
             listViewPdfs.Items.Add(Info.FullName);
         }
+        public string ImagePath { get; set; }
 
         private void listViewPdfs_DragEnter(object sender, DragEventArgs e)
         {
@@ -38,22 +40,40 @@ namespace AudioBook1
         }
         public string AllText { get; set; }
         public string FileName { get; set; }
+        string jpegdir = System.IO.Path.GetDirectoryName(@"Picture\Picture");
+        public void ConvertPDFtoImage()
+        {
+            SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();
+            f.OpenPdf(FileName);
+            f.ImageOptions.Dpi = 200;
+
+            //string jpegfile = System.IO.Path.Combine(jpegdir,
+            //    $"{FileName} Page {7}.jpg");
+            pictureBox1.Image = f.ToDrawingImage(20);
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
         private void listViewPdfs_SelectedIndexChanged(object sender, EventArgs e)
         {
-            FileName = listViewPdfs.SelectedItems[0].Text;                     
+            FileName = listViewPdfs.SelectedItems[0].Text;
             PdfReader reader = new PdfReader(FileName);
             StringBuilder sb = new StringBuilder();
-            for (int i = 1; i < 7; i++)
+            for (int i = 1; i < 2; i++)
             {
                 sb.Append(PdfTextExtractor.GetTextFromPage(reader, i));
-                       
+
             }
             SpeechSynthesizer speech = new SpeechSynthesizer();
             speech.SetOutputToDefaultAudioDevice();
-            
-            AllText = sb.ToString();reader.Close();
-            speech.Speak(AllText);
-            
+            AllText = sb.ToString(); reader.Close();
+            MessageBox.Show("Test");
+            ConvertPDFtoImage();
+
+
+            //speech.Speak(AllText);
+
+
+
         }
     }
 }
