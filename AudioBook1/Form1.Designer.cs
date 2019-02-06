1b1a6535b577172a3a39a9638477517fd55461c6
camalzade_elvin <camalzade_elvin@outlook.com>
2019-02-06 19:46:04 +0400

    DragDrop is ready . . .

--- AudioBook1/Form1.Designer.cs
+++ AudioBook1/Form1.Designer.cs
@@ -28,13 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listViewPdfs = new System.Windows.Forms.ListView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listViewPdfs
+            // 
+            this.listViewPdfs.AllowDrop = true;
+            this.listViewPdfs.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.listViewPdfs.Location = new System.Drawing.Point(23, 23);
+            this.listViewPdfs.Name = "listViewPdfs";
+            this.listViewPdfs.Size = new System.Drawing.Size(205, 381);
+            this.listViewPdfs.TabIndex = 0;
+            this.listViewPdfs.UseCompatibleStateImageBehavior = false;
+            this.listViewPdfs.SelectedIndexChanged += new System.EventHandler(this.listViewPdfs_SelectedIndexChanged);
+            this.listViewPdfs.DragDrop += new System.Windows.Forms.DragEventHandler(this.listViewPdfs_DragDrop);
+            this.listViewPdfs.DragEnter += new System.Windows.Forms.DragEventHandler(this.listViewPdfs_DragEnter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(276, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(317, 381);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(669, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listViewPdfs);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listViewPdfs;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- AudioBook1/Form1.cs
+++ AudioBook1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,28 @@ namespace AudioBook1
         public Form1()
         {
             InitializeComponent();
+        }
+        public FileInfo Info { get; set; }
+        private void listViewPdfs_DragDrop(object sender, DragEventArgs e)
+        {
+            this.BackColor = Color.DarkBlue;
+            var mydata = e.Data.GetData(DataFormats.FileDrop) as string[];
+            var Info = new FileInfo(mydata[0]);
+            listViewPdfs.Items.Add(Info.FullName);
+        }
+
+        private void listViewPdfs_DragEnter(object sender, DragEventArgs e)
+        {
+            listViewPdfs.BackColor = Color.BlueViolet;
+            e.Effect = DragDropEffects.All;
+        }
+        public string AllText { get; set; }
+        public string FileName { get; set; }
+        private void listViewPdfs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            FileName = listViewPdfs.SelectedItems[0].Text;            
+            var read = File.ReadAllText(FileName);
         }
     }
 }
